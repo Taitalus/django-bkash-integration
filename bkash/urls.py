f04938f76e8865bbbef0a5b782a542f89cd00ca2
Md. Nazmul Hossain <30841411+MdNazmul9@users.noreply.github.com>
2021-05-22 17:01:38 +0600

    updated urls

--- bkash/urls.py
+++ bkash/urls.py
@@ -9,3 +9,5 @@ urlpatterns = [
     path('payment/execute', views.PaymentExecuteApiView.as_view(), name='payment_execute_api_view'),
 ]
 
+#http://127.0.0.1:8000/bkash/payment/create
+#http://127.0.0.1:8000/bkash/payment/execute
